79002310f5d61b7cbe2c45591ffccf1ed337146a
Alternatif Yatırım <alternatifgloball@gmail.com>
2025-06-13 00:01:15 +0300

    React client eklendi

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">Temu Alışverişe Hoş Geldiniz</h1>
+    </div>
+  );
+}
+
+export default App;
